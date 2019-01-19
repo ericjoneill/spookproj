860215e9cc7598fa3948b22fbea29fe7c10f35ab
ericjoneill <42359370+ericjoneill@users.noreply.github.com>
2019-01-18 22:03:58 -0500

    Add files via upload

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -184,7 +184,7 @@ class ItemDetails extends Component {
               </div>
 
               <p className="item-details-header-info-container-content__detail">{this.props.peopleDetails ? `${this.props.peopleDetails.gender === 1 ? 'Female' : 'Male'}` : ''} | {this.props.peopleDetails ? this.props.peopleDetails.known_for_department : ''}</p>
-              <p className="item-details-header-info-container-content__genre">{this.props.peopleDetails.birthday ? this.formatYearOfBirth(this.props.peopleDetails.birthday) + ' | ' + this.findAge(this.props.peopleDetails.birthday) + ' yo' : ''}</p>
+              <p className="item-details-header-info-container-content__genre">{this.props.peopleDetails.birthday ? this.formatYearOfBirth(this.props.peopleDetails.birthday) + ' | Age: ' +  this.findAge(this.props.peopleDetails.birthday) : 'Age: Unknown'}</p>
 
             </div>
 
